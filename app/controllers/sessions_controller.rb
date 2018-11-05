f9f503c7587906fd903e9edc5a50b827e066ed17
louismn <louis.martindunord@eemi.com>
2018-11-05 15:30:09 +0100

    petite avance

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	
+  	user = User.find_by(email: params[:session][:email].downcase)
+  	
+	  if user && user.authenticate(params [:session][:password])
+	  		log_in user
+	  		redirect_to user
+	  	else 
+	  		flash.now[:danger] = "invalid email/password combinaison"
+	  	render 'new'
+	  	end
+  end
+
+    def logged_in?
+    !current_user.nil?
+  end
 end
